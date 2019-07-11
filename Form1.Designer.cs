689b8a444d4ba04fc4b5e36428a7cad0c15c0984
Arashdeep Wander <wandeara@sheridancollege.ca>
2019-07-10 20:21:45 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCourse = new System.Windows.Forms.Label();
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.lblComment = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.txtComment = new System.Windows.Forms.TextBox();
+            this.cmbSelect = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblSelect = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.addToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(9, 41);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lblCourse
+            // 
+            this.lblCourse.AutoSize = true;
+            this.lblCourse.Location = new System.Drawing.Point(9, 67);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(40, 13);
+            this.lblCourse.TabIndex = 1;
+            this.lblCourse.Text = "Course";
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.AutoSize = true;
+            this.lblGrade.Location = new System.Drawing.Point(9, 93);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(36, 13);
+            this.lblGrade.TabIndex = 2;
+            this.lblGrade.Text = "Grade";
+            // 
+            // lblComment
+            // 
+            this.lblComment.AutoSize = true;
+            this.lblComment.Location = new System.Drawing.Point(9, 119);
+            this.lblComment.Name = "lblComment";
+            this.lblComment.Size = new System.Drawing.Size(51, 13);
+            this.lblComment.TabIndex = 3;
+            this.lblComment.Text = "Comment";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(72, 41);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(201, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Location = new System.Drawing.Point(72, 67);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(201, 20);
+            this.txtCourse.TabIndex = 5;
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Location = new System.Drawing.Point(72, 93);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(201, 20);
+            this.txtGrade.TabIndex = 6;
+            // 
+            // txtComment
+            // 
+            this.txtComment.Location = new System.Drawing.Point(72, 119);
+            this.txtComment.Name = "txtComment";
+            this.txtComment.Size = new System.Drawing.Size(201, 20);
+            this.txtComment.TabIndex = 7;
+            // 
+            // cmbSelect
+            // 
+            this.cmbSelect.FormattingEnabled = true;
+            this.cmbSelect.Location = new System.Drawing.Point(356, 83);
+            this.cmbSelect.Name = "cmbSelect";
+            this.cmbSelect.Size = new System.Drawing.Size(219, 21);
+            this.cmbSelect.TabIndex = 11;
+            this.cmbSelect.SelectedIndexChanged += new System.EventHandler(this.CmbSelect_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 156);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(591, 195);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // lblSelect
+            // 
+            this.lblSelect.AutoSize = true;
+            this.lblSelect.Location = new System.Drawing.Point(382, 56);
+            this.lblSelect.Name = "lblSelect";
+            this.lblSelect.Size = new System.Drawing.Size(173, 13);
+            this.lblSelect.TabIndex = 13;
+            this.lblSelect.Text = "Select Student to Update or Delete";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem1,
+            this.deleteToolStripMenuItem1,
+            this.updateToolStripMenuItem1,
+            this.clearToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(615, 24);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // addToolStripMenuItem1
+            // 
+            this.addToolStripMenuItem1.Name = "addToolStripMenuItem1";
+            this.addToolStripMenuItem1.Size = new System.Drawing.Size(41, 20);
+            this.addToolStripMenuItem1.Text = "Add";
+            this.addToolStripMenuItem1.Click += new System.EventHandler(this.AddToolStripMenuItem1_Click);
+            // 
+            // deleteToolStripMenuItem1
+            // 
+            this.deleteToolStripMenuItem1.Name = "deleteToolStripMenuItem1";
+            this.deleteToolStripMenuItem1.Size = new System.Drawing.Size(52, 20);
+            this.deleteToolStripMenuItem1.Text = "Delete";
+            this.deleteToolStripMenuItem1.Click += new System.EventHandler(this.DeleteToolStripMenuItem1_Click);
+            // 
+            // updateToolStripMenuItem1
+            // 
+            this.updateToolStripMenuItem1.Name = "updateToolStripMenuItem1";
+            this.updateToolStripMenuItem1.Size = new System.Drawing.Size(57, 20);
+            this.updateToolStripMenuItem1.Text = "Update";
+            this.updateToolStripMenuItem1.Click += new System.EventHandler(this.UpdateToolStripMenuItem1_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.ClearToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 363);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.lblSelect);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cmbSelect);
+            this.Controls.Add(this.txtComment);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblComment);
+            this.Controls.Add(this.lblGrade);
+            this.Controls.Add(this.lblCourse);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblCourse;
+        private System.Windows.Forms.Label lblGrade;
+        private System.Windows.Forms.Label lblComment;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.TextBox txtGrade;
+        private System.Windows.Forms.TextBox txtComment;
+        private System.Windows.Forms.ComboBox cmbSelect;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblSelect;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment2
+{ //Arashdeep Wander 991495450
+    public partial class Form1 : Form
+    {
+        private SqlConnection con = new SqlConnection();
+
+        private string conString = "Server=DESKTOP-VIEIQBS\\SQLEXPRESS; Database=CSharpClass2;"
+               + "User=wandeara; Password=Gabziscool1";
+        private SqlCommand cmd;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RefreshAll()
+        {
+            con.ConnectionString = conString;
+            cmd = con.CreateCommand();
+
+            try
+            {
+                string query = "Select * from PersonInfo;";
+
+                cmd.CommandText = query;
+                con.Open();
+
+                SqlDataReader read = cmd.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(read);
+
+                dataGridView1.DataSource = dt;
+                cmbSelect.DisplayMember = "personName";
+                cmbSelect.ValueMember = "personID";
+                cmbSelect.DataSource = dt;
+
+                read.Close();
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message.ToString();
+                string caption = "Error!";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cmd.Dispose();
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshAll();
+        }
+
+        private void AddToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string course = txtCourse.Text;
+            string grade = txtGrade.Text;
+            string comment = txtComment.Text;
+
+            decimal mark;
+
+            if ((name == "") || (course == "") || (grade == "") || (comment == ""))
+            {
+                MessageBox.Show("Fields can not be Empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (!decimal.TryParse(grade, out mark))
+            {
+                MessageBox.Show("Enter Proper Grade", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                try
+                {
+                    con.ConnectionString = conString;
+                    cmd = con.CreateCommand();
+
+                    string query = " Insert into PersonInfo values('" + name + "', '" + course + "', '" + grade + "', '" + comment + "');";
+
+                    cmd.CommandText = query;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    string message = ex.Message.ToString();
+                    string caption = "Error!";
+                    MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    cmd.Dispose();
+                    con.Close();
+                    RefreshAll();
+                }
+            }
+        }
+
+        private void DeleteToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            con.ConnectionString = conString;
+            cmd = con.CreateCommand();
+
+            int pID = Convert.ToInt32(cmbSelect.SelectedValue);
+
+            try
+            {
+                string query = "Delete from PersonInfo where personID = " + pID;
+
+                cmd.CommandText = query;
+                con.Open();
+                cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message.ToString();
+                string caption = "Error!";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cmd.Dispose();
+                con.Close();
+                RefreshAll();
+            }
+        }
+
+        private void UpdateToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string course = txtCourse.Text;
+            string grade = txtGrade.Text;
+            string comment = txtComment.Text;
+
+            decimal mark;
+
+            if ((name == "") || (course == "") || (grade == "") || (comment == ""))
+            {
+                MessageBox.Show("Fields can not be Empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (!decimal.TryParse(grade, out mark))
+            {
+                MessageBox.Show("Enter Proper Grade", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                try
+                {
+                    con.ConnectionString = conString;
+                    cmd = con.CreateCommand();
+
+                    int pID = Convert.ToInt32(cmbSelect.SelectedValue);
+
+                    string query = "Update PersonInfo Set personName='" + name + "', Course_Name='" + course + "' , " +
+                    "Course_Grade='" + grade + "', Comments='" + comment + "' where personID= " + pID;
+
+                    cmd.CommandText = query;
+                    con.Open();
+                    cmd.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+                    string message = ex.Message.ToString();
+                    string caption = "Error!";
+                    MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    cmd.Dispose();
+                    con.Close();
+                    RefreshAll();
+                }
+            }
+        }
+
+        private void ClearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            cmbSelect.SelectedIndex = 0;
+            txtName.Text = "";
+            txtCourse.Text = "";
+            txtGrade.Text = "";
+            txtComment.Text = "";
+        }
+
+        private void CmbSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+            if (!cb.Focused)
+            {
+                return;
+            }
+
+            int pID = Convert.ToInt32(cmbSelect.SelectedValue);
+
+            string query1 = "Select personName from PersonInfo where personID=" + pID;
+            string query2 = "Select Course_Name from PersonInfo where personID=" + pID;
+            string query3 = "Select Course_grade from PersonInfo where personID=" + pID;
+            string query4 = "Select Comments from PersonInfo where personID=" + pID;
+
+            if (con.State == ConnectionState.Open)
+            {
+
+                cmd.CommandText = query1;
+                txtName.Text = cmd.ExecuteScalar().ToString();
+                cmd.CommandText = query2;
+                txtCourse.Text = cmd.ExecuteScalar().ToString();
+                cmd.CommandText = query3;
+                txtGrade.Text = cmd.ExecuteScalar().ToString();
+                cmd.CommandText = query4;
+                txtComment.Text = cmd.ExecuteScalar().ToString();
+
+                cmd.Dispose();
+                con.Close();
+            }
+            else
+            {
+                con.Open();
+
+                cmd.CommandText = query1;
+                txtName.Text = cmd.ExecuteScalar().ToString();
+                cmd.CommandText = query2;
+                txtCourse.Text = cmd.ExecuteScalar().ToString();
+                cmd.CommandText = query3;
+                txtGrade.Text = cmd.ExecuteScalar().ToString();
+                cmd.CommandText = query4;
+                txtComment.Text = cmd.ExecuteScalar().ToString();
+
+                cmd.Dispose();
+                con.Close();
+            }
+        }
+    }
+}
